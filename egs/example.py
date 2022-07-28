e244caf23808a463aa3c81f26e23753c2020ddf8
William N. Havard <william.havard@gmail.com>
2022-07-28 10:36:23 +0200

    Changed saving path

--- egs/example.py
+++ egs/example.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 
 # -----------------------------------------------------------------------------
-#   File: main.py (as part of project conversations)
+#   File: example.py (as part of project conversations)
 #   Created: 07/07/2022 14:43
 #   Last Modified: 07/07/2022 14:43
 # -----------------------------------------------------------------------------
@@ -21,24 +21,30 @@
 import os
 from glob import glob
 
-from .conversations import Conversation
-from .defaults import default_filtering_rules, default_turn_transition_rules, default_path_selection_rules
+from conversations import Conversation
+from conversations.defaults import default_filtering_rules, default_turn_transition_rules, default_path_selection_rules
 
 
 def main():
-    root_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'egs')
+    root_path = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+    plot_path = os.path.join(root_path, 'plots')
+    csv_path = os.path.join(root_path, 'csv')
+
+    os.makedirs(plot_path, exist_ok=True)
+    os.makedirs(csv_path, exist_ok=True)
+
     files = [f for f in os.listdir(root_path) if f.endswith('.csv')]
 
     # Define what a conversation is
     conversation = Conversation(# Participants
                                 target_participant='CHI',
-                                interactants = ['FEM'],
                                 # Connectivity
                                 allowed_gap=1000,
-                                allow_segment_jump=True,
+                                allow_segment_jump=False,
                                     # Arguments for the user-defined/default_* functions
                                 allow_multi_unit_turns=True,
                                 allow_interactions_btw_interactants=True,
+                                selection_key=['num_turns', 'num_multi_turns_transitions'],
                                 # User-defined rules
                                     # Define how turns are allowed to take place
                                 turn_transition_rules=default_turn_transition_rules,
@@ -60,14 +66,15 @@ def main():
         # Iterate over all interactional sequences found
         for idx, interactional_sequence in enumerate(interactional_sequences):
             # And plot a graph representing the interactional sequence!
-            interactional_sequence.render(dirpath=os.path.join(root_path, 'plots'),
+            interactional_sequence.render(dirpath=plot_path,
                                           name="{}_{}".format(file, idx),
                                           format='png',
-                                          delete_gv=True,
-                                          raw_with_best_path=True)
+                                          delete_gv=False,
+                                          raw_with_best_path=False)
             print('\tPlotted interactional sequence #{}'.format(idx))
 
-        interactional_sequences.to_csv(os.path.join(root_path, 'plots', '{}_interactional_sequences.csv'.format(file)))
+        output_fn = '{}_interactional_sequences.csv'.format(os.path.splitext(file)[0])
+        interactional_sequences.to_csv(os.path.join(csv_path, output_fn))
 
 
 if __name__ == '__main__':
